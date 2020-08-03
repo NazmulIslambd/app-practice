728a202bcc54d264d77b077df9bb0af4e57fd42c
NAZMUL <md.nazmulislam787@gmail.com>
2020-08-03 07:30:07 +0300

    floating bar & snack bar added

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,49 @@ class MyHome extends StatelessWidget {
       title: "Flutter Long List",
       home: SafeArea(
           child: Scaffold(
-              appBar: AppBar(
-                title: Text("Long List View"),
-              ),
-              body: getListView())),
+        appBar: AppBar(
+          title: Text("Long List View"),
+        ),
+        body: getListView(),
+
+
+        //  This code declear for floating Button
+
+        /* floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB Clicked");
+          },
+          child: Icon(Icons.add),
+          tooltip: 'Add One more Item',*/
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB Clicked");
+          },
+          child: Icon(Icons.add),
+          tooltip: 'Add one More Item',
+        ),
+      )),
     );
   }
+}
+//  function writing for create SnackBar
+
+void showSnackBar(BuildContext context, String listItem) {
+  var snackBar = SnackBar(
+    content: Text("$listItem was tapped"),
+
+    //  action use for show button on Snackbar
+    action: SnackBarAction(
+        label: "UNDO",
+        onPressed: () {
+          debugPrint("Performing dummy UNDO operation");
+        }),
+  );
+
+  // scaffold declear for show snackbar
+
+  Scaffold.of(context).showSnackBar(snackBar); //always declaer beneth FloatingActionButton
 }
 
 List<String> getListElement() {
@@ -34,6 +71,9 @@ Widget getListView() {
           title: Text(listItem[index]),
           onTap: () {
             debugPrint('${listItem[index]} was tapped');
+
+//         here we declear  this code for collect items
+            showSnackBar(context, listItem[index]);
           },
         );
       });
